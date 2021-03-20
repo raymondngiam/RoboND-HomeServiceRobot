b25366083a438120751ae3d470340e9a6aa3bae0
raymondngiam <ha.ngiam87@gmail.com>
2021-03-20 13:57:09 +0000

    Update mapping sh file

--- catkin_ws/src/scripts/mapping.sh
+++ catkin_ws/src/scripts/mapping.sh
@@ -3,6 +3,6 @@ xterm  -e "cd $(pwd)/../..; source devel/setup.bash ; roslaunch my_robot world.l
 sleep 5
 xterm  -e "cd $(pwd)/../..; source devel/setup.bash ; rosrun teleop_twist_keyboard teleop_twist_keyboard.py " &
 sleep 2
-xterm  -e "cd $(pwd)/../..; source devel/setup.bash ; rosrun gmapping slam_gmapping " &
+xterm  -e "cd $(pwd)/../..; source devel/setup.bash ; roslaunch my_robot slam_gmapping.launch " &
 sleep 2
-xterm -e "cd $(pwd)/../..; source devel/setup.bash ; rosrun rviz rviz " &
+xterm -e "cd $(pwd)/../..; source devel/setup.bash ; roslaunch my_robot rviz.launch " &
